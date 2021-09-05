b0c3f6f2e4b81fb9977c639d46c4105e7d5aeb18
venkata <sandeepsandy2530@gmail.com>
2021-09-05 21:28:09 +1000

    Form Creation for product as product-review.

--- vue/main.js
+++ vue/main.js
@@ -374,6 +374,9 @@ Vue.component('product',{
             </button>
 
         </div> 
+        <hr></hr>
+        <product-review @review-submitted="addReview"></product-review>
+        <hr></hr>
     </div>
    `,
    data(){
@@ -400,6 +403,7 @@ Vue.component('product',{
                 variantQuantity: 50
             }
         ],
+        reviews: [],
        }
    },
    methods:{
@@ -413,8 +417,10 @@ Vue.component('product',{
             this.$emit('remove-at-cart', this.variants[this.selectedVariant].variantId)
         },
         updateProduct(index){
-            this.selectedVariant = index
-            
+            this.selectedVariant = index 
+        },
+        addReview(productReview){
+            this.reviews.push(productReview)
         }
     },
     computed:{
@@ -437,6 +443,59 @@ Vue.component('product',{
             return 2.99
         }
     }
+})
+
+Vue.component('product-review',{
+    template:`
+    <form class="review-form" @submit.prevent="onSubmit">
+        <h1>Product Review Form:</h1>
+        <p>
+        <label for="name">Name:</label>
+        <textarea id="name" v-model="name"></textarea>
+        </p>
+        
+        <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+        </p>
+
+        <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+        </select>
+        </p>
+        
+        <p>
+        <input type="submit" value="Submit">
+        </p>
+    </form>
+
+    `,
+    data(){
+        return{
+            name: null,
+            review: null,
+            rating: null,
+        }
+    },
+    methods:{
+        onSubmit(){
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                reating: this.rating
+            }
+            this.$emit('review-submitted', productReview)
+            this.name=null
+            this.review=null
+            this.rating = null
+        }
+    },
 
 })
 
